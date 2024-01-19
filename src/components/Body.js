185862239fc709d51218feb846fcb1ac51af2fa8
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2024-01-19 12:27:00 +0530

    all installation done

---
+++ src/components/Body.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Body = () => {
+    
+  return (
+    <>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    <div className="text-red-500">Body</div>
+    
+    </>
+  )
+}
+
+export default Body
